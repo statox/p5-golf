ab158065aed4a0676ed741bab7b92b90e334e32e
Adrien Fabre <adrien.fabre@dashlane.com>
2023-07-13 18:09:06 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
+    import Game from '$lib/components/Game.svelte';
+</script>
+
+<Tabs>
+    <TabList>
+        <Tab>Game</Tab>
+    </TabList>
+
+    <TabPanel>
+        <Game />
+    </TabPanel>
+</Tabs>
