80275d10c6a60482863d98e669ec4861ca8b28c9
Adrien Fabre <adrien.fabre@dashlane.com>
2023-10-14 18:25:33 +0200

    Update P5Template

--- src/lib/components/P5Template.svelte
+++ src/lib/components/P5Template.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { createPhysicObjects } from '$lib/engine';
-    import type { Sphere } from '$lib/engine/Geometry';
+    import type { Sphere, Line } from '$lib/engine/Geometry';
     import { mouseIsPressedOnScreen, screenToWorldScale, worldToScreenScale } from '$lib/services/p5utils';
     import type p5 from 'p5';
     import P5, { type Sketch } from 'p5-svelte';
@@ -9,13 +9,21 @@
 
     console.clear();
     let _p5: p5;
-    const s1 = createPhysicObjects({
+    const sphere = createPhysicObjects({
         geometry: {
             type: 'sphere',
             r: 10
         } as Sphere,
         position: new Victor(30, 50),
     });
+    const line = createPhysicObjects({
+        geometry: {
+            type: 'line',
+            vector: new Victor(10, 0)
+        } as Line,
+        position: new Victor(60, 50)
+    });
+
     const SCALE = 6;
     const sketch: Sketch = (p5) => {
         const worldDimensions = new Victor(100, 100)
@@ -25,19 +33,44 @@
             p5.createCanvas(screenDimensions.x, screenDimensions.y);
             p5.noFill();
         };
+
+        let selected: string | undefined;
         p5.draw = () => {
             p5.background(0);
 
-            if (mouseIsPressedOnScreen(p5)) {
+            if (!mouseIsPressedOnScreen(p5)) {
+                selected = undefined;
+            } else {
                 const worldPos = screenToWorldScale(new Victor(p5.mouseX, p5.height - p5.mouseY), SCALE) as Victor;
-                if (worldPos.distance(s1.position) <= (s1.geometry as Sphere).r) {
-                    s1.position.copy(worldPos);
+
+                if (!selected) {
+                    if (worldPos.distance(sphere.position) <= (sphere.geometry as Sphere).r) {
+                        selected = 'sphere';
+                    } else if (worldPos.distance(line.position) <= 6) {
+                        selected = 'linestart';
+                    } else if (worldPos.distance((line.geometry as Line).vector.clone().add(line.position)) <= 6) {
+                        selected = 'lineend';
+                    }
+                }
+
+                if (selected === 'sphere') {
+                    sphere.position.copy(worldPos);
+                } else if (selected === 'linestart') {
+                    line.position.copy(worldPos);
+                } else if (selected === 'lineend') {
+                    const diff = worldPos.clone().subtract(line.position);
+                    (line.geometry as Line).vector.copy(diff);
                 }
             }
 
             p5.stroke(255);
-            const s1pos = worldToScreenScale(s1.position, SCALE) as Victor;
-            p5.circle(s1pos.x, p5.height - s1pos.y, 2 * (s1.geometry as Sphere).r * SCALE);
+            const spherepos = worldToScreenScale(sphere.position, SCALE) as Victor;
+            p5.circle(spherepos.x, p5.height - spherepos.y, 2 * (sphere.geometry as Sphere).r * SCALE);
+
+            p5.stroke(255);
+            const {x, y} = worldToScreenScale(line.position, SCALE) as Victor;
+            const linevec = worldToScreenScale((line.geometry as Line).vector, SCALE) as Victor;
+            p5.line(x, p5.height - y, x + linevec.x,  p5.height - (y + linevec.y));
         };
 
     };
