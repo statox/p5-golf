968e08baac22b248bda814e3d53de3f9eabb621b
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-01 14:52:38 +0100

    Tweak Forces draft

--- src/lib/components/drafts/Forces.svelte
+++ src/lib/components/drafts/Forces.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { World, createPhysicObjects } from '$lib/engine';
+    import { World, createPhysicObjects, type PhysicObject } from '$lib/engine';
     import type { Sphere, Line } from '$lib/engine/Geometry';
     import { dragAndDropObject, drawWorldDebug, worldToScreenScale } from '$lib/services/p5utils';
     import type p5 from 'p5';
@@ -9,22 +9,16 @@
 
     console.clear();
     let _p5: p5;
-    const s1 = createPhysicObjects({
-        geometry: {
-            type: 'sphere',
-            r: 5
-        } as Sphere,
-        position: new Victor(10, 90),
-        velocity: new Victor(0, 0)
-    });
-    const s2 = createPhysicObjects({
-        geometry: {
-            type: 'line',
-            vector: new Victor(20, 0)
-        } as Line,
-        position: new Victor(50, 50),
-        fixed: true
-    });
+    const objects: PhysicObject[] = [];
+    for (let i=0; i<10; i++) {
+        objects.push(createPhysicObjects({
+            geometry: {
+                type: 'sphere',
+                r: 0.5
+            } as Sphere,
+            position: new Victor(50, 50)
+        }));
+    }
 
     const SCALE = 6;
     let world: World;;
@@ -38,16 +32,39 @@
             world = new World({
                 enableGravity: false,
                 enableCollisions: false,
-                dimensions: worldDimensions,
-                drag: 0
+                enableOverlaps: true,
+                dimensions: worldDimensions
             });
 
-            world.addObjects([s1, s2]);
+            world.addObjects(objects);
         };
 
         const selectionState = { selectedId: undefined, selectedPart: undefined };
+        const MAX_VEL = 20;
         p5.draw = () => {
             p5.background(0);
+            for (const o of world.objects) {
+                if (o.position.x < 0 || o.position.y < 0 || o.position.x > worldDimensions.x || o.position.y > worldDimensions.y) {
+                    o.position = new Victor(Math.random() * worldDimensions.x, Math.random() * worldDimensions.y);
+                    o.velocity = new Victor(Math.random() * 20 - 10, Math.random() * 20 - 10);
+                }
+                if (o.velocity.length() > MAX_VEL) {
+                    o.velocity.normalize().multiplyScalar(MAX_VEL);
+                }
+            }
+            // randomWobble();
+            for (let i=0; i<objects.length; i++) {
+                for (let j=0; j<objects.length; j++) {
+                    if (i === j) {
+                        continue;
+                    }
+                    const s1 = objects[i];
+                    const s2 = objects[j];
+
+                    repulsion(s1, s2);
+                    attraction(s1, s2);
+                }
+            }
             world.step();
 
             dragAndDropObject(p5, world, selectionState);
@@ -55,6 +72,37 @@
         };
 
     };
+
+    const randomWobble = (o: PhysicObject) => {
+        const x = _p5.noise(_p5.frameCount);
+        const y = _p5.noise(_p5.frameCount + 1231023);
+        const diff = new Victor(x * 2 -1, y * 2 -1);
+        o.position.add(diff);
+    }
+
+    const attraction = (o1: PhysicObject, o2: PhysicObject) => {
+        const F = o2.position.clone().subtract(o1.position);
+        const d = o2.position.distance(o1.position);
+        const G = 5000;
+
+        const Fmag = G / (d*d);
+        F.norm().multiplyScalar(Fmag);
+        world.applyForce(o1, F);
+    };
+
+    const repulsion = (o1: PhysicObject, o2: PhysicObject) => {
+        const d = o2.position.distance(o1.position);
+        if (d > 100) {
+            return;
+        }
+        const F = o1.position.clone().subtract(o2.position);
+        const G = 5000;
+
+        const Fmag = G / (d*d);
+        F.norm().multiplyScalar(Fmag);
+        world.applyForce(o1, F);
+    };
+
     onDestroy(() => {
         _p5?.remove();
     });
